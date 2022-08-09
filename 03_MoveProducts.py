fc2dff64085ffce5ffd67a0f6ea2e1fe6708285e
initze <ingmarnitze@gmail.com>
2022-08-09 12:53:17 +0200

    added move files to products script

---
+++ 03_MoveProducts.py
@@ -0,0 +1,38 @@
+import argparse
+import sys
+import logging
+from processing_utils import *
+from utils_postprocessing import *
+import importlib
+import rasterio
+
+# ignore warnings
+import warnings
+
+warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--settings", default=Path('MACS_00_Settings.py'),
+                    type=Path,
+                    help="Path to Settings file")
+parser.add_argument("-d", "--destination", default=Path(r'S:\p_macsprocessing\data_products'),
+                    type=Path,
+                    help="Path to Settings file")
+args = parser.parse_args()
+
+module_name = args.settings.stem
+settings = importlib.import_module(module_name)
+
+###### START ###
+
+def main():
+    product_dir = Path(settings.PROJECT_DIR) / '06_DataProducts'
+    assert product_dir.exists()
+    output_dir = args.destination / settings.PIX4d_PROJECT_NAME
+
+    assert not output_dir.exists(), "Output directory already exists"
+    shutil.copytree(product_dir, output_dir)
+
+if __name__ == "__main__":
+    main()
