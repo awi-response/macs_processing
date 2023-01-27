63940777ce71e64e5c38b5c91efa5e0ebbc3496a
initze <ingmarnitze@gmail.com>
2023-01-27 15:14:12 +0100

    functional clipping

--- 07_Clip_PointClouds_from_Archive.py
+++ 07_Clip_PointClouds_from_Archive.py
@@ -1,8 +1,12 @@
 import argparse
+import shutil
+
+from joblib import Parallel, delayed
+
 from utils_postprocessing import *
+from tqdm import tqdm
 
 # warnings.filterwarnings('ignore')
-from utils_postprocessing import unzip_tarfile
 
 parser = argparse.ArgumentParser()
 
@@ -36,8 +40,6 @@ if not isinstance(dir_list, list):
 
 def main():
     """
-    # 2. read footprints file, get bounds, las2las
-
     # 3. move files to product dir
 
     # 4. delete intermediate files
@@ -57,9 +59,47 @@ def main():
     site_name = p_file.stem
     unzip_tarfile(p_file, site_name, target_dir=args.processing_dir)
 
+    # setup paths
+    ortho_product_list, point_cloud_dir, point_cloud_nir, point_cloud_rgb = setup_PCclip_paths(site_name)
+
+    # run clipping RGB
+    print('Start clipping point clouds')
+    Parallel(n_jobs=40)(
+        delayed(clip_las_file)(ortho_file, point_cloud_rgb, point_cloud_dir, product_name='PointCloudRGB') for ortho_file in tqdm(ortho_product_list[:]))
+    Parallel(n_jobs=40)(
+        delayed(clip_las_file)(ortho_file, point_cloud_nir, point_cloud_dir, product_name='PointCloudNIR') for
+        ortho_file in
+        tqdm(ortho_product_list[:]))
+
+    final_dir = args.output_dir / site_name / '06_DataProducts'
+    shutil.move(point_cloud_dir, final_dir)
+
     return 0
+
+
+def setup_PCclip_paths(site_name):
+    data_products_dir = args.processing_dir / site_name / '06_DataProducts'
+    footprints_file = list(data_products_dir.glob('*.geojson'))[0]
+    #
+    raster_products_dir = args.output_dir / site_name
+    ortho_product_list = list((raster_products_dir / 'Ortho').glob(f'{site_name}*.tif'))
+    point_cloud_dir = args.processing_dir / site_name / '04_pix4d' / site_name / '2_densification' / 'point_cloud'
+    point_cloud_rgb = list(point_cloud_dir.glob('*group1_densified_point_cloud*.las'))[0]
+    point_cloud_nir = list(point_cloud_dir.glob('*NIR_densified_point_cloud*.las'))[0]
+    # create tiles
+    point_cloud_dir = args.processing_dir / site_name / '06_DataProducts' / 'PointClouds'
+    os.makedirs(point_cloud_dir, exist_ok=True)
+    return ortho_product_list, point_cloud_dir, point_cloud_nir, point_cloud_rgb
+
+
+def clip_las_file(ortho_file, point_cloud, point_cloud_dir, product_name='PointCloudRGB'):
+    outfile = ortho_file.stem.replace('Ortho', product_name) + '.las'
+    point_cloud_tile = point_cloud_dir / outfile
+    with rasterio.open(ortho_file) as src:
+        min_x, min_y, max_x, max_y = src.bounds
+        s = f'las2las -keep_xy {min_x} {min_y} {max_x} {max_y} -i {point_cloud} -o {point_cloud_tile}'
+        os.system(s)
 
 
 if __name__ == "__main__":
     main()
-
