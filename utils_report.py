43d1929e6673292b1dfb7d331b38703232757abc
initze <ingmarnitze@gmail.com>
2023-04-03 16:49:45 +0200

    code cleanup and moved files to utils

--- utils_report.py
+++ utils_report.py
@@ -20,7 +20,7 @@ def split_name_details(df):
     return df.join(pd.DataFrame(df.project_name.str.split('_').to_list(), columns=['region', 'site', 'date', 'spatial_resolution', 'subset']))
 
 
-def file_check(row):
+def file_check(row, dirs=['DSM', 'Ortho', 'processing_info'], extensions=['*.tif', '*.tif.ovr','*.log', '*_nav.txt', '*_report.pdf']):
     """
     Check if the specified directories and files exist.
 
@@ -41,15 +41,24 @@ def file_check(row):
             - The number of files with a .tif, .tif.ovr, .las, .log, _nav.txt or _report.pdf extension in the processing_info directory.
     """
     outcols = []
-    for d in ['DSM', 'Ortho', 'PointClouds', 'processing_info']:
+    for d in dirs:
         data_dir = (row['products_dir'] / d)
         has_dir = data_dir.exists()
         outcols.append(has_dir)
-        extensions = ['*.tif', '*.tif.ovr', '*.las', '*.log', '*_nav.txt', '*_report.pdf']
         ex = [list(data_dir.glob(e)) for e in extensions]        
         n_files = len(flatten(ex))
         outcols.append(n_files)
     return outcols
+
+
+def file_check_PC(row, dirs=['PointClouds'], extensions=[ 'PointCloudRGB', 'PointCloudNIR']):
+    outcols = []
+    for d in dirs:
+        data_dir = (row['products_dir'] / d)
+        has_dir = data_dir.exists()
+        ex = [len(list(data_dir.glob(f'*_{e}_*'))) for e in extensions]
+        outcols.append(ex)
+    return outcols[0]
 
 
 def check_files_previews(row):
@@ -62,3 +71,36 @@ def check_files_vrt(row):
 
 def check_files_footprints(row):
     return (row['products_dir'] / (row['project_name'] + f'_tile_footprints.geojson')).exists()
+
+
+def check_file_count(df):
+    df['valid_count_dsm_ortho_equal'] = df['DSM_n_files'] == df['Ortho_n_files']
+    df['valid_count_pcrgb_pcnir_equal'] = df['PointCloudsRGB_n_files'] == df['PointCloudsNIR_n_files']
+    df['valid_count_pc_raster_equal'] = df['PointCloudsRGB_n_files']*2 == df['Ortho_n_files']
+    return df
+
+
+def color_negative_red(val):
+    if not isinstance(val, float):
+        return ''
+    else:
+        color = 'red' if val > 0 else 'white'
+        return f'background-color: {color}'
+    
+    
+def color_orange(val):
+    if not isinstance(val, float):
+        return ''
+    else:
+        color = 'orange' if val == 0 else 'white'
+        return f'background-color: {color}'
+    
+    
+def highlight_zero(val):
+    color = 'red' if val == 0 else None
+    return f'background-color: {color}'
+
+
+def highlight_invalid(row):
+    color = '#FFA500' if row['all_valid'] == False else 'white'
+    return [f'background-color: {color}' for _ in row]
