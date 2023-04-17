80ab1c8b05d55ec777af94b72515c7834614514c
initze <ingmarnitze@gmail.com>
2023-04-17 14:42:38 +0200

    updated to default posix paths

--- create_vrt.py
+++ create_vrt.py
@@ -5,6 +5,6 @@ product_types = ['Ortho', 'DSM']
 for product in product_types:
     flist = list(Path(product).glob('*.tif'))
     with open('flist.txt', 'w') as filetext:
-        [filetext.write(str(f) + '\n') for f in flist[:]]
+        [filetext.write(f.as_posix() + '\n') for f in flist[:]]
     os.system(f'gdalbuildvrt -input_file_list flist.txt {product}.vrt')
     os.remove('flist.txt')
