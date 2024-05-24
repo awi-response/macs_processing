98b2eba3c858e5aa8a95af88ab993ecbb4bb7b16
initze <ingmarnitze@gmail.com>
2024-05-24 13:36:06 +0200

    emptied default settings and minore code cleanup

--- 10_PostprocessingWrapper.py
+++ 10_PostprocessingWrapper.py
@@ -1,16 +1,18 @@
-import os
 import subprocess
 from pathlib import Path
 
 # set target dir here
-target_dir = Path(r'./test')
+target_dir = Path(r'')
 
 # insert settings files here
-settings_files = ['WA_CapeBlossom_20210625_20cm_99.py']
+settings_files = []
 
 if target_dir == Path(r'') or settings_files == []:
     raise ValueError('Please set target directory and Input settings files!')
-# TODO: change to subprocess.call() --> if output == 0, then call move script
+if not target_dir.exists():
+    raise ValueError('Target directory does not exist!')
+
+# iterate over subsets
 for settings_file in settings_files:
     # setup commandline runs
     run_string = f'python 02_Postprocessing.py -m -s {settings_file}'
