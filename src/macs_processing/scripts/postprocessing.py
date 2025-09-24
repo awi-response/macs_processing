c6c9b8074387b229569e1ab05c24b607f7f3d81c
initze <ingmarnitze@gmail.com>
2025-09-24 16:28:53 +0200

    mosaic creation moved to python functions, some refactoring and import improvement of postprocessing script

--- src/macs_processing/scripts/postprocessing.py
+++ src/macs_processing/scripts/postprocessing.py
@@ -5,6 +5,7 @@ import sys
 # ignore warnings
 import warnings
 
+from macs_processing.utils.conversions import create_mask_vector_rasterio
 from macs_processing.utils.loading import import_module_as_namespace
 from macs_processing.utils.postprocessing import *
 from macs_processing.utils.processing import *
